aa22ba5acf4e690f005164d5ded817d1ef1be8ef
anamika-yadav64 <anamikayadavyadav85@gmail.com>
2026-01-17 20:44:35 +0530

    Delete day02 directory

--- day02/task2_teck_stack_awareness.py
+++
@@ -1,8 +0,0 @@
-# Front-end technologies list
-frontend = ["HTML", "CSS", "JavaScript", "React"]
-# Back-end technologies list
-backend = ["Python", "Django", "Node.js", "MySQL"]
-print("Front-end Technologies:")
-print(frontend)
-print("\nBack-end Technologies:")
-print(backend)
