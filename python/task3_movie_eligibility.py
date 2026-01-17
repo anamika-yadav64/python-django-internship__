a9aa74ee6ae57f33adc6c013b56ab246ede61770
anamika-yadav64 <anamikayadavyadav85@gmail.com>
2026-01-17 20:46:10 +0530

    Delete python directory

--- python/task3_movie_eligibility.py
+++
@@ -1,7 +0,0 @@
-# Entry Check System
-age = int(input("Enter your age: "))
-id_card = input("Do you have an ID card? (yes/no): ").lower()
-if age >= 18 and id_card == "yes":
-    print("Entry Allowed ")
-else:
-    print("Entry Denied ")
--- python/task6_find_first_multiple.py
+++
@@ -1,6 +0,0 @@
-# Find First Multiple of 7
-
-for num in range(1, 51):
-    if num % 7== 0:
-        print("First number divisible by 7 is:", num)
-        break
